67bac32efcc15e3ff64661a81a74a6eee4dc944d
Luke Davids <lukedavids@gmail.com>
2025-07-16 09:26:00 -0500

    updates

--- data/payment-types.js
+++ data/payment-types.js
@@ -1,29 +1,29 @@
 import { fetchWithResponse, fetchWithoutResponse } from "./fetcher";
 
 export function getPaymentTypes() {
-  return fetchWithResponse('paymenttypes', {
+  return fetchWithResponse("payment-types", {
     headers: {
-      Authorization: `Token ${localStorage.getItem('token')}`
-    }
-  })
+      Authorization: `Token ${localStorage.getItem("token")}`,
+    },
+  });
 }
 
 export function addPaymentType(paymentType) {
-  return fetchWithResponse(`paymenttypes`, {
-    method: 'POST',
+  return fetchWithResponse(`payment-types`, {
+    method: "POST",
     headers: {
-      Authorization: `Token ${localStorage.getItem('token')}`,
-      'Content-Type': 'application/json'
+      Authorization: `Token ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(paymentType)
-  })
+    body: JSON.stringify(paymentType),
+  });
 }
 
 export function deletePaymentType(id) {
   return fetchWithoutResponse(`payment-types/${id}`, {
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
-      Authorization: `Token ${localStorage.getItem('token')}`
-    }
-  })
+      Authorization: `Token ${localStorage.getItem("token")}`,
+    },
+  });
 }
